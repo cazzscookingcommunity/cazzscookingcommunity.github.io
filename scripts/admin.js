8d8c675733c17486e0b5151e5250531f4aaf3d2c
chris cullin <cooking@miplace.com>
2022-01-05 09:42:27 +1100

    update files before submodule delete

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,7 +1,8 @@
 var files2display = [];
 var $recipelist;
-var recipedir = "/recipes/";
-var srcfile = "/xml/recipeList.xml";
+const recipedir = "/recipes/";
+const srcfile = "/xml/recipeList.xml";
+const recipexsd = "/xml/recipe.xsd";
 
 
 $(document).ready(function(){
@@ -27,28 +28,15 @@ function getStatus(filename) {
 
 
 function createFileList() {
-    console.debug($recipelist);
     files = $recipelist.find('filename');
-    console.debug(files);
-    // recipes = $recipelist.find("recipe");
-    // console.debug($recipelist.find("category"));
     for ( let i = 0; i < files.length; i ++ ) {
-    // for ( let i = 0; i < 10; i ++ ) {
         filename = files[i].innerHTML;
-        console.debug(filename);
         files2display.push({name: filename, status:getStatus(filename)});
     };
-
-    console.debug(files2display);
-    console.debug(files2display[0]);
-    console.debug(files2display[0].name);
-    console.debug(files2display[0].status);
     
     files2display.sort(function(a, b){
         let x = a.name.toLowerCase();
         let y = b.name.toLowerCase();
-        // console.debug(a);
-        // console.debug(b);
         if (x < y) {return -1;}
         if (x > y) {return 1;}
         return 0;
@@ -78,21 +66,19 @@ function displayList() {
     
     tabledata = "";
 
+    tabledata += `<a href=${recipexsd}>CREATE NEW RECIEPE</a>`;    
     tabledata += `<table>
-    <tr>
-        <th>recipe file</th>
-        <th>xml valid?</th>
-    </tr>`;
+        <tr>
+            <th>Update Existing Recipe</th> <th>xml valid?</th>
+        </tr>`;
 
-    console.debug(files2display);
-    console.debug(files2display.length);
+    
+
     
     for ( i = 0; i < files2display.length; i++ ) {
         const filename =  files2display[i].name;
         const xmlstatus = files2display[i].status;
-        // console.debug(`filename: ${filename}`);
         file = recipedir + filename;
-        // console.debug(`file: ${file}`);
         tabledata += `<tr><td><a href=${file}>${filename}</a></td>`;
         tabledata += `<td>${xmlstatus}</td></tr>`;
     }
