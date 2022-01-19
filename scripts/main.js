8b23c9a24338489efa231c574502d75426d36596
chris cullin <cooking@miplace.com>
2022-01-19 18:57:11 +1100

    fixed search

--- scripts/main.js
+++ scripts/main.js
@@ -50,9 +50,6 @@ $(document).ready(function(){
         // fetchMeal($recipeList, 'u');
     })
 
-    // Fetch content after 3s
-    // setTimeout(getData(['u', 'r']), 1000);
-
 });
 
 // Get recipe list based on search input using enter rather than click
@@ -70,29 +67,26 @@ $(document).on('click','.mealCardRecipeBtn',function(){
     $meal = $( meal ); // format = jquery
 
     // fetchMeal($meal,'u');
-    console.debug($meal);
     recipeFilename = $meal.find('filename').text()
-    console.debug( recipeFilename );
-    location.assign(`/pages/recipe.html?recipe=${recipeFilename}`)
-    // window.scrollTo(0,$('#random').offset().top);
-    // $('#dynamicTitle').text($meal.find('title').first().text());
+    location.assign(`/recipe.html?recipe=${recipeFilename}`)
+
 });
 
 
-// aget Categories for pre-defined search filters 
+// get recipe Categories for pre-defined search filters 
 function getCategorys($recipe, cssSelector, newlist) {
     x = $recipe.find(cssSelector);
     for ( let i = 0; i < x.length; i++ ) {
         y = x[i].innerHTML.split(" ");  // handle space separated elements like <title>
         for ( let j = 0; j < y.length; j++ ) {
-            cat = y[j].replace(",", "");
-            cat = cat.replace("(", "");
-            cat = cat.replace(")", "");
-            cat = cat.replace("*", "");
-            cat = cat.replace("!", "");
-            cat = cat.replace("-", "");
+            cat = y[j];
+            // cat = y[j].replace(",", "");
+            // cat = cat.replace("(", "");
+            // cat = cat.replace(")", "");
+            // cat = cat.replace("*", "");
+            // cat = cat.replace("!", "");
+            // cat = cat.replace("-", "");
             if ( ! ( cat == "" ) ) {
-                // newlist.add(cat.toUpperCase());
                 newlist.add(cat);
             }
         }
@@ -107,7 +101,7 @@ function addToTree(value, title) {
     }
 }
 
-// aget Categories for pre-defined search filters 
+// get lables for search tree 
 function getSearchValues($recipe, cssSelector) {
     keywords = $recipe.querySelectorAll(cssSelector);
     title = $recipe.getElementsByTagName('title')[0].innerHTML;
@@ -122,34 +116,21 @@ function getSearchValues($recipe, cssSelector) {
 // Build search structure
 function createSearchTree($recipeList) {
     for ( i = 0; i < $recipeList.length; i++ ) {
-        getSearchValues($recipeList[i], "category, diet, meal, title");
+        getSearchValues($recipeList[i], "category, diet, title");
     }
 }    
 
-// Uses the fetch() to request recipe
+// Uses the fetch() to renadom recipe
 function fetchMeal($meal, type){
     let url = '';
     if ( type == 'r' ) {
         randomNo = Math.floor(Math.random() * $meal.length );
         recipeFilename = $meal[randomNo].getElementsByTagName("filename")[0].innerHTML;
         url = (path + recipeFilename);
+        location.assign(`/recipe.html?recipe=${recipeFilename}`)
     } else {
-        url = (path + $meal.find('filename').text());
+        console.error("invalid type requested");
     }
-
-    fetch(url)
-    .then(res => res.text())
-    .then(res => new DOMParser().parseFromString(res, "text/xml"))
-    .then(res => {
-        recipe = res.querySelectorAll('recipe');
-        $recipe = $( recipe );
-        createMeal($recipe, 'r');
-        // setCache(res, type);
-    })
-    .catch( e => console.warn(e) );
-
-    $('section#random').show();
-    window.scrollTo(0,$('#random').offset().top)
 }
  
 
@@ -173,8 +154,8 @@ const getCookie = (key) => {
 }
 
 // Function to get cache data if it exists, otherwise, fetch from the API
-const getData = (types) => {
-    console.log("not yet implement");
+// const getData = (types) => {
+//     console.log("not yet implement");
     // types.forEach(type => {
     //     if( type === "u" ) {
     //         let mealData = JSON.parse(sessionStorage.getItem(type));
@@ -192,11 +173,31 @@ const getData = (types) => {
     //         mealData !== null ? createMeal(mealData, type) : fetchMeal(type);
     //     }
     // })
+// }
+
+
+// search title, category, diet for a regex term
+function searchRecipeList( searchTerm ) {
+    var shortlist = []
+    for ( i = 0; i < recipeList.length; i++ ) {
+        $recipe = $( recipeList[i] );
+        recipetitle = $recipe.find('title').text();
+        title = $recipe.find('title').text().toLowerCase().indexOf(searchTerm.toLowerCase());
+        category = $recipe.find('category').text().toLowerCase().indexOf(searchTerm.toLowerCase());
+        diet = $recipe.find('diet').text().toLowerCase().indexOf(searchTerm.toLowerCase());
+        
+        if ( ( title > -1 ) || (category > -1) || (diet > -1) ) {
+            shortlist.push(recipetitle)
+        }
+    }
+    return(shortlist);
 }
 
 // fetch meals matching searchTerm
 function fetchCategoryMeal(searchTerm){
-    let recipeNameShortList = searchTree[searchTerm.toUpperCase()];
+    // let recipeNameShortList = searchTree[searchTerm.toUpperCase()];
+    const recipeNameShortList = searchRecipeList(searchTerm);
+
     $("#errorMessageContainer").remove();
     $('#searchRecipe').val(searchTerm);
     $('#userInput').text(searchTerm);
@@ -215,129 +216,6 @@ function fetchCategoryMeal(searchTerm){
         window.scrollTo(0,$('#mealCardsSection').offset().top-100);
     }
 }
-
-
-// Function to generate the random meal UI component
-const createMeal = ($meal, type) => {
-
-    // clear any existing display data
-    $('#randomMealMetadata').empty();
-    $('#randomMealInstructions').empty();
-    $('#randomMealImg').empty();
-    // $('#dynamicTitle').empty();
-    
-
-    // Set meal thumbnail
-    setMealThumbnail($meal,type);
-
-    let mealMetadata = '', mealInstr = '';
-
-    // Fill meal name 
-    mealMetadata += `<span>Name:</span>
-        ${$meal.find('title').first().text()} 
-        <br/>`
-
-    // Fill category 
-    mealMetadata += `<span>Category:</span> 
-        ${$meal.find("category").text()}
-        <br/>`
-
-    // Fill ingredients
-    let ingredients = [];
-    getIngredients($meal, ingredients);
-    if ( ingredients.length > 0 ) {
-        mealMetadata += `<br><span>Ingredients:</span><br>
-        ${ingredients.join('')}`
-        // <ul> ${ingredients.join('')} </ul>`
-    }
-
-    // Set instructions
-    let instructions = [];
-    getInstructions($meal, instructions);
-    mealInstr =`<span>Instructions:</span><br>
-     ${instructions.join('')}`;
-    
-    if ( type === 'r') { 
-        $('section#random').show();
-        $('#randomMealMetadata').html(mealMetadata); 
-        $('#randomMealInstructions').html(mealInstr); 
-        console.debug(window.location.href);
-    }
-}
-
-// structure instructions into list
-const setInstructionList = (steps, outputStr) => {
-    let i = 0;
-    outputStr.push('<ul>');
-    for( i = 0; i < steps.length; i++ ){
-        step = steps[i].innerHTML
-        outputStr.push(`<p>step ${i}:<br/>${steps[i].innerHTML}</p>`);
-    }
-    outputStr.push('</ul>');
-}
-
-// Gets instruction steps
-const getInstructions = ($meal, outputStr) => {
-    let instructionsArr = [];
-    // check if recipe has parts 
-    let parts = $meal.find("part");
-    if ( parts.length > 1 ) {
-        for ( let i = 0; i < parts.length; i++ ) {
-            title = parts[i].getElementsByTagName('title')[0].innerHTML;
-            outputStr.push(`<strong><u>${title}</u></strong>`);
-            instructionsArr = parts[i].getElementsByTagName("step");
-            setInstructionList(instructionsArr, outputStr);
-        }
-    } else {
-        instructionsArr = $meal.find("step");
-        setInstructionList(instructionsArr, outputStr);
-    }
-}
-
-// structure ingredients into list
-const setIngredientList = (ingredientsArr, outputStr) => {
-    let i = 0;
-    outputStr.push('<ul>');
-    for( i = 0; i < ingredientsArr.length; i++ ){
-        ingredient = ingredientsArr[i].innerHTML
-        outputStr.push(`<li>${ingredient}</li>`);
-    }
-    if ( !( i % 2 === 0 )) { 
-        outputStr.push('<li> </li>'); 
-    }
-    outputStr.push('</ul>');
-}
-
-
-// Gets ingredients of the random meal
-const getIngredients = ($meal,outputStr) => { 
-    console.log($meal);
-    let ingredientsArr = [];
-    // check if recipe has parts 
-    let parts = $meal.find("part");
-    if ( parts.length > 1 ) {
-        for ( let i = 0; i < parts.length; i++ ) {
-            title = parts[i].getElementsByTagName('title')[0].innerHTML;
-            outputStr.push(`<strong><u>${title}</u></strong>`);
-            ingredientsArr = parts[i].getElementsByTagName("ingredient");
-            setIngredientList(ingredientsArr, outputStr);
-        }
-    } else {
-        ingredientArr = $meal.find("ingredient");
-        setIngredientList(ingredientArr, outputStr);
-    }
-}  
-
-// get meal thumbnail image
-const setMealThumbnail = ($meal,type) => {
-    const img = $meal.find("thumbnail").text();
-    const title = $meal.find("title").text();
-    const file = $meal.find('filename').text();
-
-    let imgSrc = `<img src="${img}" alt="${title} thumbnail" onclick="window.open('${path}${file}')"/>`;
-    if ( type === 'r') { $('#randomMealImg').html(imgSrc); }
-}
-
 
 // Create meal cards for shortlisted recipes
 function createMealCards(shortlist) {
--- scripts/recipe.js
+++ scripts/recipe.js
@@ -29,6 +29,126 @@ function loadRecipe(recipeFile){
         $('#dynamicTitle').text($recipe.find('title').first().text());
     })
     .catch( e => console.warn(e) );
+}
 
-  
+
+// Function to generate the random meal UI component
+const createMeal = ($meal, type) => {
+
+    // clear any existing display data
+    $('#randomMealMetadata').empty();
+    $('#randomMealInstructions').empty();
+    $('#randomMealImg').empty();
+    // $('#dynamicTitle').empty();
+    
+
+    // Set meal thumbnail
+    setMealThumbnail($meal,type);
+
+    let mealMetadata = '', mealInstr = '';
+
+    // Fill meal name 
+    mealMetadata += `<span>Name:</span>
+        ${$meal.find('title').first().text()} 
+        <br/>`
+
+    // Fill category 
+    mealMetadata += `<span>Category:</span> 
+        ${$meal.find("category").text()}
+        <br/>`
+
+    // Fill ingredients
+    let ingredients = [];
+    getIngredients($meal, ingredients);
+    if ( ingredients.length > 0 ) {
+        mealMetadata += `<br><span>Ingredients:</span><br>
+        ${ingredients.join('')}`
+        // <ul> ${ingredients.join('')} </ul>`
+    }
+
+    // Set instructions
+    let instructions = [];
+    getInstructions($meal, instructions);
+    mealInstr =`<span>Instructions:</span><br>
+     ${instructions.join('')}`;
+    
+    if ( type === 'r') { 
+        $('section#random').show();
+        $('#randomMealMetadata').html(mealMetadata); 
+        $('#randomMealInstructions').html(mealInstr); 
+        console.debug(window.location.href);
+    }
+}
+
+// structure instructions into list
+const setInstructionList = (steps, outputStr) => {
+    let i = 0;
+    outputStr.push('<ul>');
+    for( i = 0; i < steps.length; i++ ){
+        step = steps[i].innerHTML
+        outputStr.push(`<p>step ${i}:<br/>${steps[i].innerHTML}</p>`);
+    }
+    outputStr.push('</ul>');
+}
+
+// Gets instruction steps
+const getInstructions = ($meal, outputStr) => {
+    let instructionsArr = [];
+    // check if recipe has parts 
+    let parts = $meal.find("part");
+    if ( parts.length > 1 ) {
+        for ( let i = 0; i < parts.length; i++ ) {
+            title = parts[i].getElementsByTagName('title')[0].innerHTML;
+            outputStr.push(`<strong><u>${title}</u></strong>`);
+            instructionsArr = parts[i].getElementsByTagName("step");
+            setInstructionList(instructionsArr, outputStr);
+        }
+    } else {
+        instructionsArr = $meal.find("step");
+        setInstructionList(instructionsArr, outputStr);
+    }
+}
+
+// structure ingredients into list
+const setIngredientList = (ingredientsArr, outputStr) => {
+    let i = 0;
+    outputStr.push('<ul>');
+    for( i = 0; i < ingredientsArr.length; i++ ){
+        ingredient = ingredientsArr[i].innerHTML
+        outputStr.push(`<li>${ingredient}</li>`);
+    }
+    if ( !( i % 2 === 0 )) { 
+        outputStr.push('<li> </li>'); 
+    }
+    outputStr.push('</ul>');
+}
+
+
+// Gets ingredients of the random meal
+const getIngredients = ($meal,outputStr) => { 
+    console.log($meal);
+    let ingredientsArr = [];
+    // check if recipe has parts 
+    let parts = $meal.find("part");
+    if ( parts.length > 1 ) {
+        for ( let i = 0; i < parts.length; i++ ) {
+            title = parts[i].getElementsByTagName('title')[0].innerHTML;
+            outputStr.push(`<strong><u>${title}</u></strong>`);
+            ingredientsArr = parts[i].getElementsByTagName("ingredient");
+            setIngredientList(ingredientsArr, outputStr);
+        }
+    } else {
+        ingredientArr = $meal.find("ingredient");
+        setIngredientList(ingredientArr, outputStr);
+    }
+}  
+
+// get meal thumbnail image
+const setMealThumbnail = ($meal,type) => {
+    const img = $meal.find("thumbnail").text();
+    const title = $meal.find("title").text();
+    const file = $meal.find('filename').text();
+
+    let imgSrc = `<img src="${img}" alt="${title} thumbnail" onclick="window.open('${path}${file}')"/>`;
+    if ( type === 'r') { $('#randomMealImg').html(imgSrc); }
 }
