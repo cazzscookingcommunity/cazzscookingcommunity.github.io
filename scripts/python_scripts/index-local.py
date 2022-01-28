f69d1481f6cdae980b9ac03ce7f7392543ca6306
chris cullin <cooking@miplace.com>
2022-01-28 22:05:18 +1100

    created sitemap and updated workflow

--- scripts/python_scripts/index-local.py
+++ scripts/python_scripts/index-local.py
@@ -2,13 +2,15 @@
 
 import os
 import sys
+import datetime
+date = datetime.datetime.now().date()
+print(date)
+
 
 # source info
 recipedir = "./recipes/"
 
 # output info
-outputpath = "./xml/"
-outputfile = "recipeList.xml"
 xmlheader = '''<?xml version="1.0" encoding="UTF-8"?>
 
 <recipeList
@@ -23,6 +25,22 @@ xmlfooter = '''
 </recipeList>
 '''
 
+sitemap_header = '''<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+'''
+
+sitemap_page = '''
+    <url>
+        <loc>http://cazzscookingcommunity.github.io/recipe.html?recipe={}</loc>
+        <lastmod>{}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>'''
+
+
+sitemap_footer = '''
+</urlset>'''
+
 def loadfile(filename):
     ifile = open(filename, "rt")
     istr = ifile.read()
@@ -30,53 +48,62 @@ def loadfile(filename):
     # print(istr)
     return istr
 
-def initOutput(file):
+def initOutput(file, content):
     with open(file, "w") as dst:
-        dst.write(xmlheader)
+        dst.write(content)
         dst.close()
 
-def closeOutput(file):
+def closeOutput(file, content):
     with open(file, "a") as dst:
-        dst.write(xmlfooter)
+        dst.write(content)
         dst.close()
 
 
-def processFile(inputfile):
-    with open(outputpath + outputfile, "a") as dst:
-        with open(recipedir + inputfile, "rt") as src:
-            dst.write("<recipe>\n")
-            firstTitle = True
-            while True:
-                line = src.readline()
-                if not line:
-                    break
+def processFile(filename, recipelist, sitemap):
+    with open(recipelist, "a") as dst:
+        with open(sitemap, "a") as sm:
+            with open(recipedir + filename, "rt") as src:
+                sm.write(sitemap_page.format(filename, date))
+                dst.write("<recipe>\n")
+                firstTitle = True
+                while True:
+                    line = src.readline()
+                    if not line:
+                        break
 
-                if firstTitle and "<title>" in line:
-                    dst.write(line)
-                    dst.write("    <filename>" + inputfile + "</filename>\n")
-                    firstTitle = False
-                    continue
-
-                if ("<category>"  in line or
-                    "<diet>"      in line or
-                    "<thumbnail>" in line or
-                    "<image>"     in line): 
+                    if firstTitle and "<title>" in line:
                         dst.write(line)
+                        dst.write("    <filename>" + filename + "</filename>\n")
+                        firstTitle = False
+                        continue
 
-            dst.write("</recipe>\n")
-            dst.close()
-            src.close()
+                    if ("<category>"  in line or
+                        "<diet>"      in line or
+                        "<thumbnail>" in line or
+                        "<image>"     in line): 
+                            dst.write(line)
+
+                dst.write("</recipe>\n")
+                dst.close()
+                sm.close()
+                src.close()
             
 
 
 if __name__== "__main__":
-    initOutput(outputpath + outputfile)
+    recipelist = "./xml/recipeList.xml"
+    sitemap = "sitemap.xml"
+
+    initOutput(recipelist, xmlheader)
+    initOutput(sitemap, sitemap_header)
+
     directory = os.fsencode(recipedir)
     
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
-        if filename.endswith(".xml") and filename != outputfile: 
+        if filename.endswith(".xml"): 
             print(filename)
-            processFile(filename)
+            processFile(filename, recipelist, sitemap)
 
-    closeOutput(outputpath + outputfile)
+    closeOutput(recipelist, xmlfooter)
+    closeOutput(sitemap, sitemap_footer)
