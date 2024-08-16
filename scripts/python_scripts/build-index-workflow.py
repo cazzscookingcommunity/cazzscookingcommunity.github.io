d0a131ed015ff02323638c384c16607882ce8270
chris cullin <cooking@miplace.com>
2024-08-16 12:06:17 +1000

    updates to github action py scripts for html

--- scripts/python_scripts/build-index-workflow.py
+++ scripts/python_scripts/build-index-workflow.py
@@ -1,10 +1,10 @@
 #!/bin/python3
 
-""" Updates index, sitemap and recipe lists
+""" Updates indexhtml, sitemap.xml and creates a search_index.json file
 
-When a new recipe has been added or a recipe updated VIA WEBSITE
-this script is run BY GITHUB WORKFLOW to generate
-new sitemap.xml and recipeList.xml files.
+When a new recipe has been added or a recipe updated
+this script is run BY GITHUB WORKFLOW to generate and updates files
+listed above
 """
 
 import os
@@ -16,32 +16,15 @@ import datetime
 date = datetime.datetime.now().date()
 
 # input files
-recipedir = "recipes/xml/"
-index = "index.html"
+xmlRecipeDir = "recipes/xml/"
+indexHtmlFile = "index.html"
 
 
 # output files
 searchIndex = "components/search_index.json"
 sitemap = "sitemap.xml"
-# outputpath = "./xml/"
-# outputfile = "recipeList.xml"
 
-# XML output template.  Blank lines are part of the template.
-schema = xmlschema.XMLSchema('./xml/recipe.xsd')
-# xmlheader = '''<?xml version="1.0" encoding="UTF-8"?>
-
-# <recipeList
-# xmlns="https://cazzscookingcommunity.github.io"
-# xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-# xsi:schemaLocation="https://cazzscookingcommunity.github.io ../xml/recipeList.xsd">
-
-# '''
-
-# xmlfooter = '''
-
-# </recipeList>
-# '''
-
+# SITEMAP.XML TEMPLATE
 sitemap_header = '''<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
@@ -76,6 +59,7 @@ sitemap_page = '''
 sitemap_footer = '''
 </urlset>'''
 
+
 def loadfile(filename):
     ifile = open(filename, "rt")
     istr = ifile.read()
@@ -100,25 +84,12 @@ def get_file_modified_date(file_path):
     modified_date = datetime.datetime.fromtimestamp(timestamp).date()
     return modified_date
 
-# def extract_image_name(file_path):
-#     try:
-#         tree = etree.parse(file_path)
-#         root = tree.getroot()
-#         namespace = {'ns': 'https://cazzscookingcommunity.github.io'}
-#         # Find the <thumbnail> tag with namespace handling
-#         image = root.find('.//ns:thumbnail', namespaces=namespace).text
-#         return image
-#     except:
-#         print("\n*********1")
-#         print("ERROR getting image: " + file_path + " Skipping\n")
-#         return
-
 
 def parse_recipe(filename):
     """Parse an XML recipe file and extract relevant fields."""
     try:
         # Parse the XML file
-        tree = etree.parse(recipedir + filename)
+        tree = etree.parse(xmlRecipeDir + filename)
         root = tree.getroot()
 
         # Define the namespace
@@ -127,10 +98,9 @@ def parse_recipe(filename):
         htmlFilename = root.find("ns:htmlFilename", namespace).text if root.find("ns:htmlFilename", namespace) is not None else ""
         thumbnail = root.find("ns:thumbnail", namespace).text if root.find("ns:thumbnail", namespace) is not None else ""
                 
-
         # Extract fields from the XML
         recipe = {
-            "id": os.path.basename(recipedir + filename),
+            "id": os.path.basename(xmlRecipeDir + filename),
             "title": root.find("ns:title", namespace).text if root.find("ns:title", namespace) is not None else "",
             "filename": root.find("ns:filename", namespace).text if root.find("ns:filename", namespace) is not None else "",
             "htmlFilename": htmlFilename,
@@ -141,8 +111,7 @@ def parse_recipe(filename):
             # "steps": " ".join([elem.text for elem in root.findall("ns:step", namespace) if elem.text])
         }
         
-
-        fileDate = get_file_modified_date(recipedir + filename) 
+        fileDate = get_file_modified_date(xmlRecipeDir + filename) 
         recipeXML = sitemap_page.format(htmlFilename, thumbnail, fileDate)
 
                     
@@ -151,65 +120,22 @@ def parse_recipe(filename):
         print(f"Error parsing {filename}: {e}")
         return None
 
-
-# def processFile(inputfile, searchIndex, sitemap):
-#     try:
-#         validRecipe = schema.is_valid(recipedir + filename)
-#     except:
-#         print("\n*********1")
-#         print("ERROR: " + filename + " is not a valid recipe.  Skipping\n")
-#         # exit(1)
-#         return
-
-#     if schema.is_valid(recipedir + filename):
-#         with open(searchIndex, "a") as dst:
-#             with open(sitemap, "a") as sm:
-#                 with open(recipedir + inputfile, "rt") as src:
-#                     sm.write(sitemap_page.format(filename, imagename, fileDate))
-#                     dst.write("<recipe>\n")
-#                     firstTitle = True
-#                     while True:
-#                         line = src.readline()
-#                         if not line:
-#                             break
-
-#                         if firstTitle and "<title>" in line:
-#                             dst.write(line)
-#                             dst.write("    <filename>" + inputfile + "</filename>\n")
-#                             firstTitle = False
-#                             continue
-
-#                         if ("<category>"  in line or
-#                             "<diet>"      in line or
-#                             "<thumbnail>" in line or
-#                             "<image>"     in line): 
-#                                 dst.write(line)
-
-#                     dst.write("</recipe>\n")
-#                     dst.close()
-#                     sm.close()
-#                     src.close()
-#     else:
-#         print("\n*********2")
-#         print("ERROR: " + filename + " is not a valid recipe.  Skipping\n")
-#         return
-
-        
+     
 def main(): 
     searchIndexData = []
-    date = get_file_modified_date(index)
+    date = get_file_modified_date(indexHtmlFile)
 
+    # initialise the output files
     initOutput(searchIndex, "")
     initOutput(sitemap, sitemap_header.format(date, date))
 
                         
-    directory = os.fsencode(recipedir)
-    
+    directory = os.fsencode(xmlRecipeDir)
     for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        
+        filename = os.fsdecode(file) 
         if filename.endswith(".xml"): 
             recipe, recipeXML = parse_recipe(filename)
+            
             if recipe:
                 searchIndexData.append(recipe)
 
@@ -224,6 +150,9 @@ def main():
     closeOutput(searchIndex, "")
     closeOutput(sitemap, sitemap_footer)
     print("indexing complete")
+
+
+
 
 if __name__== "__main__":
     main()
