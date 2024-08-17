8e13147920b6202c33ee6f437301b77a99beb6ca
chris cullin <cooking@miplace.com>
2024-08-17 16:35:42 +1000

    changes to static pages and github actions

---
+++ scripts/python_scripts/generateStaticAllRecipesPage.py
@@ -0,0 +1,92 @@
+import os
+import json
+from bs4 import BeautifulSoup
+
+
+# inputs
+recipeDir = "recipes/html/"
+imageDir = "recipes/images/"
+template_file = 'allRecipes-template.html'
+index_file = 'components/search_index.json'
+
+# outputs
+output_file = 'staticAllRecipes.html'
+
+
+
+def create_meal_cards(allRecipes):
+    meal_cards = ''
+    
+    for recipe in allRecipes:
+        img = recipe.get('thumbnail', "")
+        file = recipe.get('htmlFilename', "")
+        
+        category_tags = ''
+        if recipe.get('category'):
+            category_tags = ' '.join(
+                f'<a href="#mealCardsSection" class="tag" onclick="searchByTag(\'{tag.strip()}\')">#{tag.strip()}</a>'
+                for tag in recipe['category'].split()
+            )
+        
+        diet_tags = ''
+        if recipe.get('diet'):
+            diet_tags = ' '.join(
+                f'<a href="#mealCardsSection" class="tag" onclick="searchByTag(\'{tag}\')">#{tag}</a>'
+                for tag in recipe['diet'].split()
+            )
+        
+        meal_cards += (
+            f'<div class="cards four columns">'
+            f'    <div class="card">'
+            f'        <a href="{recipeDir}{file}">'
+            f'            <img src="{imageDir}{img}" alt="{recipe.get("title", "")} thumbnail" class="u-max-full-width mealCardRecipeBtn" />'
+            f'        </a>'
+            f'        <div class="card-body recipe-action" display="none">'
+            f'            <div class="cardTitle">{recipe.get("title", "")}</div>'
+            f'            <div class="cardTags">{category_tags} {diet_tags}</div>'
+            f'        </div>'
+            f'    </div>'
+            f'</div>'
+        )
+        # print(pretty_format_html(meal_cards))
+        # return (meal_cards)
+
+    return meal_cards
+
+
+
+def pretty_format_html(html_string):
+    soup = BeautifulSoup(html_string, 'html.parser')
+    return soup.prettify()
+
+
+def generate_html_content(meal_cards):
+
+    # open the template file reading
+    with open(template_file, 'r') as file:
+        template = file.read()
+
+    html_content = template.format(meal_cards=meal_cards)
+    pretty_html_content = pretty_format_html(html_content)
+
+    return pretty_html_content
+    
+
+def main():
+
+    # Read the recipe index file
+    with open(index_file, 'r') as f:
+        allRecipes = json.load(f)
+
+    meal_cards = create_meal_cards(allRecipes)
+    content = generate_html_content(meal_cards)
+
+    # write the static html file
+    with open(output_file, 'w') as file:
+        file.write(content)
+    
+    print("successfully generated Static AllRecipes file")
+
+if __name__ == "__main__":
+    main()
+
--- scripts/python_scripts/generateStaticRecipePage.py
+++ scripts/python_scripts/generateStaticRecipePage.py
@@ -2,10 +2,16 @@ import os
 import json
 from lxml import etree
 import re
+from bs4 import BeautifulSoup
 
 # Define the path to the directory containing XML files and the template file
-xml_directory = 'recipes/xml/'
+recipeDir = 'recipes/xml/'
 template_file = 'recipe-template.html'
+index_file = 'components/search_index.json'
+git_changes = 'files.txt' # files.txt created by Github Actions. see .gituhub/workflows/build-recipe-index.yml
+
+
+
 output_directory = 'recipes/html/'
 # Define the namespace
 ns = {'ns': 'https://cazzscookingcommunity.github.io'}
@@ -16,6 +22,10 @@ ns = {'ns': 'https://cazzscookingcommunity.github.io'}
 with open(template_file, 'r') as file:
     template = file.read()
 
+
+def pretty_format_html(html_string):
+    soup = BeautifulSoup(html_string, 'html.parser')
+    return soup.prettify()
 
 # Utility function to format time, with error handling for blank or missing fields
 def format_time(duration):
@@ -63,17 +73,15 @@ def parse_recipe(xml_file):
         cooktime = format_time(iso_cooktime)
         servings = root.findtext('ns:yield', namespaces=ns)
         
-        # Collect all occurrences of diet and category into arrays
         diet_elements = root.findall('ns:diet', namespaces=ns)
         category_elements = root.findall('ns:category', namespaces=ns)
 
-        # Extract text if elements are found, otherwise use an empty list
+        # Ensure the variables are strings if there's only one category or diet
         diet = [d.text for d in diet_elements if d is not None and d.text]
         category = [c.text for c in category_elements if c is not None and c.text]
 
-        # Convert lists to comma-separated strings for HTML representation, if needed
-        diet = ", ".join(diet) if diet else ""  # If the list is empty, return an empty string
-        category = ", ".join(category) if category else ""
+        diet = ", ".join(diet) if diet else ""  # Treat as a string
+        category = ", ".join(category) if category else ""  # Treat as a string
 
         
         return {
@@ -146,8 +154,8 @@ def generate_html_content(recipe_data, ingredients_list, instructions_list, sche
         iso_cooktime=recipe_data['iso_cooktime'],
         cooktime=recipe_data['cooktime'],
         servings=recipe_data['servings'],
-        diet=", ".join(recipe_data['diet']),
-        category=", ".join(recipe_data['category']),
+        diet=recipe_data['diet'],
+        category=recipe_data['category'],
         ingredients=ingredients_list,
         instructions=instructions_list,
         schema_ingredients=schema_ingredients_json,
@@ -175,118 +183,52 @@ def generate_html_from_xml(xml_file):
     output_filename = os.path.join(output_directory, os.path.basename(xml_file).replace('.xml', '.html'))
     write_html_file(html_content, output_filename)
 
-def main():
-    for xml_file in os.listdir(xml_directory):
+def process_directory():
+    for xml_file in os.listdir(recipeDir):
         if xml_file.endswith('.xml'):
-            generate_html_from_xml(os.path.join(xml_directory, xml_file))
+            generate_html_from_xml(os.path.join(recipeDir, xml_file))
+    return
 
-if __name__ == "__main__":
-    main()
+def process_git_changes():
+    # get all recipes from index
+    with open(index_file, 'r') as f:
+        allRecipes = json.load(f)
 
+    with open(git_changes, 'r') as file:
+            changed_files = file.readlines()
 
-
-
-#######
-
-# Function to generate HTML for each recipe
-def generate_html_from_xml(xml_file):
-    # Parse the XML file
-    tree = etree.parse(xml_file)
-    root = tree.getroot()
-    
-    # Extract data from XML
-    title = root.findtext('ns:title', namespaces=ns)
-    # htmlFilename = os.path.basename(xml_file).replace('.xml', '.html')
-    htmlFilename = root.findtext('ns:htmlFilename', namespaces=ns)
-    filename = root.findtext('ns:filename', namespaces=ns)
-    thumbnail = root.findtext('ns:thumbnail', namespaces=ns)
-    iso_preptime = root.findtext('ns:prepTime', namespaces=ns)
-    iso_cooktime = root.findtext('ns:cookTime', namespaces=ns)
-    preptime = format_time(iso_preptime)
-    cooktime = format_time(iso_cooktime)
-    servings = root.findtext('ns:yield', namespaces=ns)
-
-    # Collect all occurrences of diet and category into arrays
-    diet_elements = root.findall('ns:diet', namespaces=ns)
-    category_elements = root.findall('ns:category', namespaces=ns)
-
-    # Extract text if elements are found, otherwise use an empty list
-    diet = [d.text for d in diet_elements if d is not None and d.text]
-    category = [c.text for c in category_elements if c is not None and c.text]
-
-    # Convert lists to comma-separated strings for HTML representation, if needed
-    diet = ", ".join(diet) if diet else ""  # If the list is empty, return an empty string
-    category = ", ".join(category) if category else ""
-
-    
-    # Handling ingredients and instructions
-    ingredients_list = ""
-    instructions_list = ""
-    schema_ingredients = []
-    schema_instructions = []
-
-    for part in root.findall('ns:part', namespaces=ns):
-        part_name = part.findtext('ns:title', namespaces=ns)
-        ingredients_list += f"<strong>{part_name}:</strong><ul>"
-        instructions_list += f"<strong>{part_name}:</strong><ol class='recipe-steps'>"
-
-        for ingredient in part.findall('ns:ingredient', namespaces=ns):
-            ingredients_list += f"<li>{ingredient.text}</li>"
-            schema_ingredients.append(ingredient.text)
-        ingredients_list += "</ul>"
-
-        for step in part.findall('ns:step', namespaces=ns):
-            instructions_list += f"<li>{step.text}</li>"
-            schema_instructions.append({"@type": "HowToStep", "text": step.text})
-        instructions_list += "</ol>"
-
-    # Handle simple recipes without parts
-    if not ingredients_list and not instructions_list:
-        ingredients_list = "<ul>"
-        instructions_list = "<ol class='recipe-steps'>"
+    # Remove 'recipes/' prefix and process each file
+    for file_path in changed_files:
+        file_name = file_name.replace(recipeDir, '')
         
-        for ingredient in root.findall('ns:ingredient', namespaces=ns):
-            ingredients_list += f"<li>{ingredient.text}</li>"
-            schema_ingredients.append(ingredient.text)
-        ingredients_list += "</ul>"
+        # Search for the recipe in the search_index
+        recipe = next((recipe for recipe in allRecipes if recipe['filename'] == file_name), None)
+        generate_html_from_xml(file_path)
+    return
 
-        for step in root.findall('ns:step', namespaces=ns):
-            instructions_list += f"<li>{step.text}</li>"
-            schema_instructions.append({"@type": "HowToStep", "text": step.text})
-        instructions_list += "</ol>"
 
-     # Convert schema data to JSON format
-    schema_ingredients_json = json.dumps(schema_ingredients)
-    schema_instructions_json = json.dumps(schema_instructions)
-    
-    # Create the HTML content
-    html_content = template.format(
-        title=title,
-        filename=filename,
-        htmlFilename=htmlFilename,
-        thumbnail=thumbnail,
-        iso_preptime = iso_preptime,
-        preptime=preptime,
-        iso_cooktime=iso_cooktime,
-        cooktime=cooktime,
-        diet=diet,
-        servings=servings,
-        category=category,
-        ingredients=ingredients_list,
-        instructions=instructions_list,
-        schema_ingredients=schema_ingredients_json,
-        schema_instructions=schema_instructions_json
-    )
-    
-    # Write the HTML file to the output directory
-    with open(os.path.join(output_directory, htmlFilename), 'w') as file:
-        file.write(html_content)
 
 def main():
-    # Crawl the recipe directory and generate HTML files for each XML file
-    for xml_file in os.listdir(xml_directory):
-        if xml_file.endswith('.xml'):
-            generate_html_from_xml(os.path.join(xml_directory, xml_file))
+    if os.path.exists(git_changes):
+        # Running on GitHub actions, so process changed files
+        print("processing git changes")
+        process_git_changes()
+    else:
+        # Running locally so process all recipes
+        print("processing all recipes")
+        process_directory()
+
+        # test individual files before do full directory
+        # testfile1="recipes/xml/easy_stir_fry.xml"
+        # testfile2="recipes/xml/lasagna.xml"
+
+        # generate_html_from_xml(testfile1)
+        # print(f"Conversion of {testfile1} is complete")
+
+        # generate_html_from_xml(testfile2)
+        # print(f"Conversion of {testfile2} is complete")
+
+
 
 if __name__== "__main__":
     main()
