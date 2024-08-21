31d00b41200132d1972f481315ab75281462bde7
chris cullin <cooking@miplace.com>
2024-08-22 08:28:44 +1000

    bug fix favroites clear and mobile css

--- scripts/my-favourites.js
+++ scripts/my-favourites.js
@@ -10,39 +10,43 @@ function toggleFavourite(recipeId) {
         console.debug("add to favourites");
     }
     localStorage.setItem('favourites', JSON.stringify(favourites));
-    if (favourites.length > 0) {
-        updateFavouriteButton(recipeId);
-    }
-    else {
-        location.reload();
+    updateFavouriteButton(recipeId);
+
+    if (favourites.length == 0) {
+         clearFavourites();
     }
 }
 
 function getFavouriteState(recipeId) {
     let favourites = JSON.parse(localStorage.getItem('favourites')) || [];
     if (favourites.includes(recipeId)) {
-        textContent = '❤️ Remove from Favourites';
+        textContent = '❤️ Remove';
     } else {
-        textContent = '❤️ Add to Favourites';
+        textContent = '❤️ Add';
     }
     return textContent
 }
 
 function updateFavouriteButton(recipeId) {
     let favourites = JSON.parse(localStorage.getItem('favourites')) || [];
-    // const button = document.querySelector(`button[onclick="toggleFavourite('${recipeId}')"]`);
     const button = document.getElementById(`${recipeId}`);
     if (favourites.includes(recipeId)) {
-        button.textContent = '❤️ Remove from Favourites';
+        button.textContent = '❤️ Remove';
     } else {
-        button.textContent = '❤️ Add to Favourites';
+        button.textContent = '❤️ Add';
     }
 }
 
 function clearFavourites() {
     favourites = [];
     localStorage.setItem('favourites', JSON.stringify(favourites));
-    location.reload();
+    
+    // if no search (i.e. displaying My Favourites) then reload
+    const savedSearchTerm = sessionStorage.getItem('searchTerm');
+    const savedResults = sessionStorage.getItem('searchResults');
+    if ( ! (savedSearchTerm && savedResults) ) {
+        location.reload();
+    }
 }
 
 // displays the search results
